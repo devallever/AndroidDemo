e4364453a690a8e708f2d1fd6b609f54f955a099
devallever <devallever@163.com>
2020-07-31 20:13:26 +0800

    全屏方案一：设置style

---
+++ app/src/main/java/com/allever/app/demo/alienscreen/AlienFullScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.allever.app.demo.alienscreen
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.allever.app.demo.R
+
+class AlienFullScreenActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alien_full_screen)
+    }
+}
