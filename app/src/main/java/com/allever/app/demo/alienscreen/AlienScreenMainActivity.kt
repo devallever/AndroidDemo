35043b43ea84903171bf6a591d1443a5e0191e51
devallever <devallever@163.com>
2020-07-31 19:15:43 +0800

    适配沉浸式状态栏，方案一：android:fitsSystemWindows="true"

--- app/src/main/java/com/allever/app/demo/alienscreen/AlienScreenMainActivity.kt
+++ app/src/main/java/com/allever/app/demo/alienscreen/AlienScreenMainActivity.kt
@@ -1,6 +1,8 @@
 package com.allever.app.demo.alienscreen
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import com.allever.app.demo.R
 
@@ -8,5 +10,11 @@ class AlienScreenMainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alien_screen_main)
+
+        // 透明状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            window.addFlags(
+                WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
     }
 }
