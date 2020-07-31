1359bd3e5a3448d268e260780e60547961cbe192
devallever <devallever@163.com>
2020-07-31 20:15:14 +0800

    全屏方案(全屏不显示状态栏)二：代码设置

--- app/src/main/java/com/allever/app/demo/alienscreen/AlienFullScreenActivity.kt
+++ app/src/main/java/com/allever/app/demo/alienscreen/AlienFullScreenActivity.kt
@@ -1,6 +1,8 @@
 package com.allever.app.demo.alienscreen
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.allever.app.demo.R
 
@@ -8,5 +10,11 @@ class AlienFullScreenActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alien_full_screen)
+
+        // 全屏显示
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
     }
 }
