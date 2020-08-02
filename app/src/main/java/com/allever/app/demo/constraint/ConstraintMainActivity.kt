dc48a35ccc54f5c64b5087e764e04252641e534f
devallever <devallever@163.com>
2020-08-03 00:35:36 +0800

    FEATURE： 约束布局

---
+++ app/src/main/java/com/allever/app/demo/constraint/ConstraintMainActivity.kt
@@ -0,0 +1,12 @@
+package com.allever.app.demo.constraint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.allever.app.demo.R
+
+class ConstraintMainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint_main)
+    }
+}
